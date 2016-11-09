c67b26ad8279712404a55cd554b478e34475d92e
hszymanski <hsszyman@gmail.com>
2016-11-09 17:32:49 -0500

    Playing around with grid layouts and screwing around with web view elements

--- theCircuitLive/theCircuitLive/MainPage.xaml.cs
+++ theCircuitLive/theCircuitLive/MainPage.xaml.cs
@@ -12,6 +12,7 @@ namespace theCircuitLive
         public MainPage()
         {
             InitializeComponent();
+            this.BackgroundColor = Color.Black;
             this.PushAsync(new StartingPage());
            
         }
@@ -23,26 +24,50 @@ namespace theCircuitLive
     {
         public StartingPage()
         {
-            var layouts = new Grid();
-            
-            
-            Content = layouts;
-            
-           
-            
+            this.BackgroundColor = Color.Black;
+            Content = this.GridLay();
             
         }
 
         public ContentView WebPage ()
         {
-
-            int i = 0;
             var web = new WebView
             {
                 Source = "https://msdn.microsoft.com/en-us/library/6kxxabwd.aspx"
                 
             };
             return new ContentView { Content = web };
+        }
+
+        public Grid GridLay()
+        {
+            Grid grid = new Grid();
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) });
+
+            
+            var page1 = WebPage();
+            var page2 = WebPage();
+            var page3 = WebPage();
+            var page4 = WebPage();
+            var page5 = WebPage();
+            var page6 = WebPage();
+
+            grid.Children.Add(page1, 0, 0);
+            grid.Children.Add(page2, 0, 1);
+            grid.Children.Add(page3, 1, 0);
+            grid.Children.Add(page4, 1, 1);
+            grid.Children.Add(page5, 0, 2);
+            grid.Children.Add(page6, 2, 0);
+
+            Grid.SetColumnSpan(page5, 3);
+            Grid.SetRowSpan(page6, 3);
+            
+            return grid;
         }
     }
 
--- theCircuitLive/theCircuitLive/Properties/AssemblyInfo.cs
+++ theCircuitLive/theCircuitLive/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("theCiruitLive")]
+[assembly: AssemblyTitle("theCircuitLive")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("theCiruitLive")]
+[assembly: AssemblyProduct("theCircuitLive")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
