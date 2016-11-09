6e863b82454727de7d47ea96a7607ab0aaeb0e3d
hszymanski <hsszyman@gmail.com>
2016-11-09 12:43:46 -0500

    first exper commit

--- theCiruitLive/theCiruitLive/MainPage.xaml.cs
+++ theCiruitLive/theCiruitLive/MainPage.xaml.cs
@@ -35,10 +35,12 @@ namespace theCircuitLive
 
         public ContentView WebPage ()
         {
+
+            int i = 0;
             var web = new WebView
             {
                 Source = "https://msdn.microsoft.com/en-us/library/6kxxabwd.aspx"
-
+                
             };
             return new ContentView { Content = web };
         }
