76c0bf0b51da13e08c4dd30a54f64e5b3951c946
hszymanski <hsszyman@gmail.com>
2016-11-28 01:30:23 -0500

    added skeleton for tap action

--- Kuromori/Kuromori/Kuromori/Pages/SpeakerView.xaml.cs
+++ Kuromori/Kuromori/Kuromori/Pages/SpeakerView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,14 @@ namespace Kuromori.Pages
             Label speakerDesc = this.FindByName<Label>("Description");
             speakerName.Text = speaker.SpeakerName;
             speakerDesc.Text = speaker.SpeakerDescription;
+
+            speakerImage.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() =>
+                {
+                    Debug.WriteLine("Test"); /// fill in what happens when image is clicked here
+                })
+            });
 
             Device.OnPlatform(null, null, () =>
            {
