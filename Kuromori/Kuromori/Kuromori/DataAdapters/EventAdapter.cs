b1a415793f157382fdf09a17a12b569b6e12c2f3
hsszyman <hsszyman@gmail.com>
2016-11-29 00:40:27 -0500

    Migrated Jackson's code to an eventAdapter method.

--- Kuromori/Kuromori/Kuromori/DataAdapters/EventAdapter.cs
+++ Kuromori/Kuromori/Kuromori/DataAdapters/EventAdapter.cs
@@ -1,11 +1,14 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
 using Kuromori.DataStructure;
 using Kuromori.InfoIO;
 
 namespace Kuromori.DataAdapters
 {
     /// <summary>
-    /// 
+    /// Utility classes for events that help for formatting, or offloading work that would be done in the 
+    /// viewmodel
     /// </summary>
     public static class EventAdapter
     {
@@ -17,6 +20,55 @@ namespace Kuromori.DataAdapters
         {
             Events events = await EventConnection.GetEventData();
             return events.EventSet;
+        }
+
+        public static string ConvertDate(string date)
+        {
+            CultureInfo provider = CultureInfo.InvariantCulture;
+            string monthNum = System.DateTime.ParseExact(date, "yyyy-MM-dd", provider).ToString("MM");
+            string month = "";
+            switch (monthNum)
+            {
+                case "01":
+                    month += "January";
+                    break;
+                case "02":
+                    month += "February";
+                    break;
+                case "03":
+                    month += "March";
+                    break;
+                case "04":
+                    month += "April";
+                    break;
+                case "05":
+                    month += "May";
+                    break;
+                case "06":
+                    month += "June";
+                    break;
+                case "07":
+                    month += "July";
+                    break;
+                case "08":
+                    month += "August";
+                    break;
+                case "09":
+                    month += "September";
+                    break;
+                case "10":
+                    month += "October";
+                    break;
+                case "11":
+                    month += "November";
+                    break;
+                case "12":
+                    month += "December";
+                    break;
+            }
+
+            return month + " " + DateTime.ParseExact(date, "yyyy-MM-dd", provider).ToString("dd, yyyy");
+            
         }
     }
 }
--- Kuromori/Kuromori/Kuromori/Pages/EventView.xaml.cs
+++ Kuromori/Kuromori/Kuromori/Pages/EventView.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Globalization;
+using Kuromori.DataAdapters;
 using Kuromori.DataStructure;
 using Kuromori.Pages;
 using Xamarin.Forms;
@@ -34,56 +35,13 @@ namespace Kuromori
             Image eventImage = this.FindByName<Image>("Image");
             eventTopic.FontSize = 16;
             eventTopic.FontAttributes = FontAttributes.Bold;
-
+            eventDate.Text = EventAdapter.ConvertDate(anEvent.EventDate); // Jacksons code <<<< 
             eventTopic.Text += anEvent.EventTopic;
             eventDesc.Text = anEvent.EventDescription;
 
             CultureInfo provider = CultureInfo.InvariantCulture;
 
-            string monthNum = DateTime.ParseExact(anEvent.EventDate, "yyyy-MM-dd", provider).ToString("MM");
-            string month = "";
-            switch(monthNum)
-            {
-                case "01":
-                    month += "January";
-                    break;
-                case "02":
-                    month += "February";
-                    break;
-                case "03":
-                    month += "March";
-                    break;
-                case "04":
-                    month += "April";
-                    break;
-                case "05":
-                    month += "May";
-                    break;
-                case "06":
-                    month += "June";
-                    break;
-                case "07":
-                    month += "July";
-                    break;
-                case "08":
-                    month += "August";
-                    break;
-                case "09":
-                    month += "September";
-                    break;
-                case "10":
-                    month += "October";
-                    break;
-                case "11":
-                    month += "November";
-                    break;
-                case "12":
-                    month += "December";
-                    break;
-            }
-
-
-            eventDate.Text = month + " " + DateTime.ParseExact(anEvent.EventDate, "yyyy-MM-dd", provider).ToString("dd, yyyy");
+           
 
             eventImage.GestureRecognizers.Add(new TapGestureRecognizer
             {
