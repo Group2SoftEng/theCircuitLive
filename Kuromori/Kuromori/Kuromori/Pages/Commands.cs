f22cde6ba75be86374583e2b3e24058b83a09013
hszymanski <hsszyman@gmail.com>
2016-11-28 19:14:42 -0500

    dont hurt

---
+++ Kuromori/Kuromori/Kuromori/Pages/Commands.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Kuromori.Pages
+{
+    public static class Commands
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static Command Refresh()
+        {
+            return new Command(new Action(() =>
+            {
+                
+            }));
+        }
+
+
+
+
+    }
+}
