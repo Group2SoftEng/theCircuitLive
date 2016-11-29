c9c47701a7bd8d8d0e8b0fa92ec7c2fdb7db9f44
Jackson <Jackson@JACKSON-PC>
2016-11-28 20:09:07 -0500

    Formatted date

--- Kuromori/Kuromori/Kuromori/Pages/EventView.xaml.cs
+++ Kuromori/Kuromori/Kuromori/Pages/EventView.xaml.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 using Kuromori.DataStructure;
 using Kuromori.Pages;
 using Xamarin.Forms;
@@ -36,8 +37,54 @@ namespace Kuromori
 
             eventTopic.Text += anEvent.EventTopic;
             eventDesc.Text = anEvent.EventDescription;
-            eventDate.Text = anEvent.EventDate;
-           
+
+            CultureInfo provider = CultureInfo.InvariantCulture;
+
+            string monthNum = DateTime.ParseExact(anEvent.EventDate, "yyyy-MM-dd", provider).ToString("MM");
+            string month = "";
+            switch(monthNum)
+            {
+                case "01":
+                    month += "January";
+                    break;
+                case "02":
+                    month += "February";
+                    break;
+                case "03":
+                    month += "March";
+                    break;
+                case "04":
+                    month += "April";
+                    break;
+                case "05":
+                    month += "May";
+                    break;
+                case "06":
+                    month += "June";
+                    break;
+                case "07":
+                    month += "July";
+                    break;
+                case "08":
+                    month += "August";
+                    break;
+                case "09":
+                    month += "September";
+                    break;
+                case "10":
+                    month += "October";
+                    break;
+                case "11":
+                    month += "November";
+                    break;
+                case "12":
+                    month += "December";
+                    break;
+            }
+
+
+            eventDate.Text = month + " " + DateTime.ParseExact(anEvent.EventDate, "yyyy-MM-dd", provider).ToString("dd, yyyy");
+
             eventImage.GestureRecognizers.Add(new TapGestureRecognizer
             {
                 Command = new Command(() =>
