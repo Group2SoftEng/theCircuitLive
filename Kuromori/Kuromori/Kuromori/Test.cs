6dfb8dacf1bfd20db4ae743e027df4e8b3723336
hszymanski <hsszyman@gmail.com>
2016-12-06 05:19:35 -0500

    final commit for iteration 0

---
+++ Kuromori/Kuromori/Kuromori/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kuromori.DataAdapters;
+using NUnit.Framework;
+using NUnit.Compatibility;
+
+namespace Kuromori
+{
+    [TestFixture]
+    class Test
+    {
+        [Test]
+        public void test()
+        { 
+            Assert.Pass(EventAdapter.ConvertDate("09 08 2015"));
+        }
+    }
+}
