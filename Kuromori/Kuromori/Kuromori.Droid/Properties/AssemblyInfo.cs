2129785efd50e8f79397a2cf78d716d1c08221c9
Jackson <Jackson@JACKSON-PC>
2016-12-06 20:54:30 -0500

    added app icon

--- Kuromori/Kuromori/Kuromori.Droid/Properties/AssemblyInfo.cs
+++ Kuromori/Kuromori/Kuromori.Droid/Properties/AssemblyInfo.cs
@@ -15,6 +15,7 @@ using Android.App;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
+[assembly: Application(Icon = "@drawable/theCircuit-IG-March_web.jpg")]
 
 // Version information for an assembly consists of the following four values:
 //
