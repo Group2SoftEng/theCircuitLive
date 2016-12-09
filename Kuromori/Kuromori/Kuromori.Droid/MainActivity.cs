15864ce43a9eaa8a2d44652806abed832b8fa8da
hszymanski <hsszyman@gmail.com>
2016-12-09 14:12:45 -0500

    changed app name. packagename is the same though

--- Kuromori/Kuromori/Kuromori.Droid/MainActivity.cs
+++ Kuromori/Kuromori/Kuromori.Droid/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Kuromori.Droid
 {
-    [Activity(Label = "Kuromori", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "TheCircuitLive", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
