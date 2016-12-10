21d74a9a086f54a61d3a6aaf35a2cb64ad2dabb5
Jackson <Jackson@JACKSON-PC>
2016-12-10 13:49:36 -0500

    changed app icon

--- Kuromori/Kuromori/Kuromori.Droid/MainActivity.cs
+++ Kuromori/Kuromori/Kuromori.Droid/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Kuromori.Droid
 {
-    [Activity(Label = "TheCircuitLive", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "TheCircuitLive", Icon = "@drawable/thecircuitappicon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
