e246afe4e6765d5417573bbe70882b43fafdc430
hsszyman <hsszyman@gmail.com>
2017-02-21 13:51:19 -0500

    added splash page and user select page

---
+++ Kuromori/Kuromori/Kuromori/Pages/UserSelectPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Kuromori
+{
+	public partial class UserSelectPage : ContentPage
+	{
+		public UserSelectPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
