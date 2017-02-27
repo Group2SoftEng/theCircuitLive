3326f16b951f3a5036e32e57fdfb131ba8226799
Jackson <Jackson@JACKSON-PC>
2017-02-27 18:59:10 -0500

    Added Profile Page styling.

--- Kuromori/Kuromori/Kuromori/Pages/ProfilePage.xaml.cs
+++ Kuromori/Kuromori/Kuromori/Pages/ProfilePage.xaml.cs
@@ -1,25 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
 
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 
 namespace Kuromori
 {
-
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ProfilePage : ContentPage
     {
         public ProfilePage()
         {
-            ProfileImage.Source = new Uri("https://inomics.com/sites/default/files/pictures/picture-95970-1460131169.png");
+            InitializeComponent();
+            ProfileImage.Source = new Uri("https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg");
         }
     }
 }
