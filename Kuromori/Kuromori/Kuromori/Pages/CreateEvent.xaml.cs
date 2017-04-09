7a32bcca8c78de8f01200421d1f1273aae9122f4
hsszyman <hsszyman@gmail.com>
2017-04-09 17:08:34 -0400

    removed speaker fields in create event page

--- Kuromori/Kuromori/Kuromori/Pages/CreateEvent.xaml.cs
+++ Kuromori/Kuromori/Kuromori/Pages/CreateEvent.xaml.cs
@@ -22,22 +22,31 @@ namespace Kuromori
             User = user;
             
         }
-        public void CreateEventClick(object sender, EventArgs args)
+        public async void CreateEventClick(object sender, EventArgs args)
         {
-            Debug.WriteLine(HttpUtils.PostInfo(new List<KeyValuePair<string, string>>
+           String n = (HttpUtils.PostInfo(new List<KeyValuePair<string, string>>
             {
                 new KeyValuePair<string, string>("event_title", Title.Text),
                 new KeyValuePair<string, string>("organizer_id", User.Id ),
                 new KeyValuePair<string, string>("event_desc", Description.Text),
                 new KeyValuePair<string, string>("event_url", RegisterURL.Text),
-                new KeyValuePair<string, string>("event_date", Date.Text),
+				new KeyValuePair<string, string>("event_date", Date.Date.ToString("yyyy-MM-dd")),
                 new KeyValuePair<string, string>("event_img", EBImage.Text),
-                new KeyValuePair<string, string>("event_topic", Topic.Text),
-                new KeyValuePair<string, string>("speaker_name", SpeakerName.Text),
-                new KeyValuePair<string, string>("speaker_desc", SpeakerDesc.Text),
-                new KeyValuePair<string, string>("speaker_img", SpeakerImg.Text)
-            }, "http://haydenszymanski.me/softeng05/create_event.php").ResponseSuccess);
-            Navigation.PopToRootAsync();
+                new KeyValuePair<string, string>("event_topic", Topic.Text)
+                //new KeyValuePair<string, string>("speaker_name", SpeakerName.Text),
+                //new KeyValuePair<string, string>("speaker_desc", SpeakerDesc.Text),
+                //new KeyValuePair<string, string>("speaker_img", SpeakerImg.Text)
+			}, "http://haydenszymanski.me/softeng05/create_event.php").ResponseInfo);
+
+			if (await DisplayAlert("Add Organizer", "Add Organizer?", "Yes", "No"))
+			{
+
+				Debug.WriteLine("hui");
+			}
+			else
+			{
+	            await Navigation.PopToRootAsync();
+			}
             /*Task.Run(async () =>
            {
                temp = await HttpUtils.GetEBEventData();
