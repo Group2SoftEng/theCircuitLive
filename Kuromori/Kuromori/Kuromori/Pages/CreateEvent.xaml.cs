8fb321b9b2b888e9e30a6bfd77a2301cbea0798a
Jackson <Jackson@JACKSON-PC>
2017-04-18 14:14:13 -0400

    Added Event image browse

--- Kuromori/Kuromori/Kuromori/Pages/CreateEvent.xaml.cs
+++ Kuromori/Kuromori/Kuromori/Pages/CreateEvent.xaml.cs
@@ -15,12 +15,101 @@ namespace Kuromori
     public partial class CreateEvent : ContentPage
     {
         User User;
+        UriImageSource ImgSrc;
+        string SrcString;
 
         public CreateEvent(User user)
         {
             InitializeComponent();
             User = user;
-            
+
+            img1.Source = new Uri("http://haydenszymanski.me/softeng05/images/ProfileImages/usr_antelope.jpeg");
+            img2.Source = new Uri("http://haydenszymanski.me/softeng05/images/ProfileImages/usr_chihuahua.jpeg");
+            img3.Source = new Uri("http://haydenszymanski.me/softeng05/images/ProfileImages/usr_duckling.jpeg");
+            img4.Source = new Uri("http://haydenszymanski.me/softeng05/images/ProfileImages/usr_horses.jpg");
+            img5.Source = new Uri("http://haydenszymanski.me/softeng05/images/ProfileImages/usr_giraffe.jpg");
+            img6.Source = new Uri("http://haydenszymanski.me/softeng05/images/ProfileImages/usr_hedgehog.jpeg");
+            img7.Source = new Uri("http://haydenszymanski.me/softeng05/images/ProfileImages/usr_bunny.jpeg");
+
+            ///<summary>
+            ///if user selects img1 create event with image to img1
+            /// </summary>
+            img1.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() =>
+                {
+                    ImgSrc = (Xamarin.Forms.UriImageSource)img1.Source;
+                    SrcString = ImgSrc.Uri.ToString();
+                })
+            });
+
+            ///<summary>
+            ///if user selects img2 update create event with image to img2
+            /// </summary>
+            img2.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() =>
+                {
+                    ImgSrc = (Xamarin.Forms.UriImageSource)img2.Source;
+                    SrcString = ImgSrc.Uri.ToString();
+                })
+            });
+            ///<summary>
+            ///if user selects img3 create event with image to img3
+            /// </summary>
+            img3.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() =>
+                {
+                    ImgSrc = (Xamarin.Forms.UriImageSource)img3.Source;
+                    SrcString = ImgSrc.Uri.ToString();
+                })
+            });
+            ///<summary>
+            ///if user selects img4 create event with image to img4
+            /// </summary>
+            img4.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() =>
+                {
+                    ImgSrc = (Xamarin.Forms.UriImageSource)img4.Source;
+                    SrcString = ImgSrc.Uri.ToString();
+                })
+            });
+            ///<summary>
+            ///if user selects img5 create event with image to img5
+            /// </summary>
+            img5.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() =>
+                {
+                    ImgSrc = (Xamarin.Forms.UriImageSource)img5.Source;
+                    SrcString = ImgSrc.Uri.ToString();
+                })
+            });
+            ///<summary>
+            ///if user selects img6 create event with image to img6
+            /// </summary>
+            img6.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() =>
+                {
+                    ImgSrc = (Xamarin.Forms.UriImageSource)img6.Source;
+                    SrcString = ImgSrc.Uri.ToString();
+                })
+            });
+            ///<summary>
+            ///if user selects img7 create event with image to img7
+            /// </summary>
+            img7.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() =>
+                {
+                    ImgSrc = (Xamarin.Forms.UriImageSource)img7.Source;
+                    SrcString = ImgSrc.Uri.ToString();
+                })
+            });
+
         }
 
 		/// <summary>
@@ -40,7 +129,7 @@ namespace Kuromori
                 new KeyValuePair<string, string>("event_desc", Description.Text),
                 new KeyValuePair<string, string>("event_url", RegisterURL.Text),
 				new KeyValuePair<string, string>("event_date", Date.Date.ToString("yyyy-MM-dd")),
-                new KeyValuePair<string, string>("event_img", EBImage.Text),
+                new KeyValuePair<string, string>("event_img", SrcString),
                 new KeyValuePair<string, string>("event_topic", Topic.Text)
                 //new KeyValuePair<string, string>("speaker_name", SpeakerName.Text),
                 //new KeyValuePair<string, string>("speaker_desc", SpeakerDesc.Text),
