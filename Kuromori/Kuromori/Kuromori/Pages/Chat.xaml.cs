6ba587218701a7d68c1ac2dcc3572b300a339d6e
dharless1 <dharless1@radford.edu>
2017-04-25 21:22:45 -0400

    Partial Implementation of Chat

---
+++ Kuromori/Kuromori/Kuromori/Pages/Chat.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Kuromori.DataStructure;
+using Kuromori;
+using Kuromori.InfoIO;
+using System.Diagnostics;
+
+namespace Kuromori.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Chat : ContentPage
+    {
+        public Chat(User current)
+        {
+            InitializeComponent();
+            List<ImageCell> ChatParticipants = new List<ImageCell>();
+
+            List<User> UserList;
+                Debug.WriteLine("DICKS");
+
+                UserList = HttpUtils.GetJsonInfo<List<User>>(new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("username", current.UserName),
+                new KeyValuePair<string, string>("password", current.Password)
+            }, "http://haydenszymanski.me/softeng05/get_chatted.php"
+           ).Result;
+                Debug.WriteLine("AHHHHHHHHHHHHHHHHHHHHHHH");
+                
+
+                foreach (User user in UserList)
+                {
+                    Debug.WriteLine(user.UserName);
+                    ImageCell temp = new ImageCell // for each participant in the list create a new cell.
+                    {
+                        ImageSource = user.ProfilePicture,
+                        Detail = user.UserName,
+                        
+                    };
+                    ChatParticipants.Add(temp);
+                }
+                listView.ItemsSource = ChatParticipants;
+                listView.ItemTapped += ChatPage;
+
+            Debug.WriteLine("end");
+
+           
+        }
+            
+          
+
+        public void ChatPage(object sender, ItemTappedEventArgs e)
+        {
+            
+        }
+
+
+
+    }
+}
---
+++ Kuromori/Kuromori/Kuromori/Pages/IndividualChat.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Kuromori.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class IndividualChat : ContentPage
+    {
+        public IndividualChat(object sender, ItemTappedEventArgs e)
+        {
+            InitializeComponent();
+
+            List<ImageCell> n = new List<ImageCell>();
+
+            //foreach(Chat chat in Chat_List)
+
+            ImageCell temp = new ImageCell // for each participant in the list create a new cell.
+            {
+                //ImageSource = "User IMAGEEEEE";
+                Text = "Username : ",               
+            };
+            n.Add(temp);
+
+            listView.ItemsSource = n;            
+
+        }
+    }
+}
